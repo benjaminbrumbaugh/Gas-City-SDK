6d162041cdbed4c4356be14d97081aae42d7bb10
Benjamin Brumbaugh <benbrumbaugh@gmail.com>
2026-09-09 00:16:52 -0700

    test(resourcecensus): baseline rescued subprocess tests

--- internal/testpolicy/resourcecensus/census.go
+++ internal/testpolicy/resourcecensus/census.go
@@ -125,8 +125,8 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeAll,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   649,
-			BaselineFiles:   188,
+			BaselineCalls:   652,
+			BaselineFiles:   189,
 			ReportedCalls:   495,
 			ReportedFiles:   135,
 			OwnerBead:       "ga-80po0c.2",
@@ -166,8 +166,8 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeUntagged,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   436,
-			BaselineFiles:   126,
+			BaselineCalls:   439,
+			BaselineFiles:   127,
 			ReportedCalls:   380,
 			ReportedFiles:   98,
 			OwnerBead:       "ga-80po0c.2",
@@ -466,8 +466,8 @@ var bootstrapPolicy = Ledger{
 		{
 			Scope:           ScopeUntagged,
 			Resource:        ResourceSubprocess,
-			BaselineCalls:   429,
-			BaselineFiles:   122,
+			BaselineCalls:   432,
+			BaselineFiles:   123,
 			ReportedCalls:   394,
 			ReportedFiles:   105,
 			OwnerBead:       "ga-80po0c.2.1",
